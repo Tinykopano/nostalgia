1fce0a14a82159e37dbf04628788fbf6c18e5930
Tinykopano <tiny_kopano@outlook.com>
2023-05-01 18:58:04 +0800

    axios

--- frontend/src/views/MovieRepository.vue
+++ frontend/src/views/MovieRepository.vue
@@ -1,18 +1,45 @@
 <template>
-    <el-row >
-    <div>电影名称：</div><el-input>22</el-input>
+    <el-row>
+        <div>电影名称：</div>
+        <el-input></el-input>
+        <el-button>查询</el-button>
     </el-row>
+
     <el-row >
-        <el-col :span="8" >7</el-col>
-        <el-col :span="8" >8</el-col>
-        <el-col :span="8" >9</el-col>
+        <el-col v-if="movies" :span="8" v-for="movie in movies " :key="movie.id">
+            <el-card>
+                <div style="padding: 14px">
+                    <span>{{ movie.chineseName}}</span>
+                </div>
+                <div style="padding: 14px">
+                    <span>{{ movie.director.chineseName}}</span>
+                </div>
+            </el-card>
+        </el-col>
     </el-row>
 </template>
 
-<script>
-export default {
-    name: "Movie"
-}
+<script setup>
+import axios from "axios";
+import { onMounted, ref} from "vue";
+
+const movies = ref([])
+axios.defaults.baseURL = 'http://localhost/nostalgia'
+
+onMounted( () => {
+    axios.get('/movies',{
+        params:{
+            pageNum: 1,
+            pageSize: 9,
+        }
+    }).then(response => {
+        movies.value = response.data.data.pages
+    }).catch(error => {
+        console.log(error);
+    })
+
+})
+
 </script>
 
 <style scoped>
